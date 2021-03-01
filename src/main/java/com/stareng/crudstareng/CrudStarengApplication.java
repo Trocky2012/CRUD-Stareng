27011826d2ad88d8a2fe7c1b1f0a67830f7df37b
Thiago Trolle <thiagotrolle@yahoo.com.br>
2021-03-01 16:02:35 -0300

    Project created

---
+++ src/main/java/com/stareng/crudstareng/CrudStarengApplication.java
@@ -0,0 +1,13 @@
+package com.stareng.crudstareng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudStarengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudStarengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stareng/crudstareng/CrudStarengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stareng.crudstareng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudStarengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
